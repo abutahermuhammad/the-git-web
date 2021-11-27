f53ce4d8cf4dd9288be84d6cda774dca485185ba
Abu Taher Muhammad <abut1081@gmail.com>
2021-11-28 02:58:11 +0600

    [TEST1]

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   reactStrictMode: true,
   images: {
     loader: 'cloudinary',
-    path: 'http://res.cloudinary.com/gisthive/',
+    path: 'https://res.cloudinary.com/gisthive/',
 	minimumCacheTTL: 6468,
   },
 }
